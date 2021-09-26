dfb36c68aa07626e114ec20ce21d6f29bee45907
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2021-09-26 16:47:13 +0500

    first page added

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import "./style.css";
+import Typography from '@mui/material/Typography';
 
 const Footer = () => {
-    return (
-        <div> I am Footer </div>
-    )
-}
+  return (
+    <div className="footer-container">
+      <div className="footer-content">
+        <Typography variant="subtitle1" >Contact Admin</Typography> 
+        <a href="https://www.google.com">
+        <img
+          src="https://cdn.worldvectorlogo.com/logos/discord-6.svg"
+          alt="dicord"
+          width="50"
+          height="50"
+          style={{marginLeft: "10px"}}
+        />
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
