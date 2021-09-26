80655a17f2015cee22a6912a025c41aff88e6f7a
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2021-09-26 16:00:21 +0500

    frontend app initalized

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+
+      
+      {/* Pre-Signin screen */}
+      {/* Check if you are whitelisted */}
+      {/* Footer and contract information */}
+
+
+      {/* Post-Signin screen */}
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
