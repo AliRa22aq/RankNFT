b361dc67c6f51b3d2c1b1ad0e0d7ae3b7921c08c
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2021-10-01 03:11:58 +0500

    Snipping Form added

--- src/components/PostSinginScreen/components/form/index.tsx
+++ src/components/PostSinginScreen/components/form/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import "./style.css";
 // import { intervalToDuration, formatDistanceToNow } from 'date-fns'
 import { useDispatch, useSelector } from 'react-redux';
@@ -10,80 +10,258 @@ import { TextField} from 'formik-material-ui';
 // import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 // import * as Yup from 'yup';
+const Web3 = require("web3");
+
+
+interface Data {
+  totalSupply: string | null, 
+  name: string | null,
+  baseTokenURI: string  | null,
+  attributes: any  |  null,
+  uri : string | null
+}
+const initialData = {
+  totalSupply:  null, 
+  name:  null,
+  baseTokenURI: null,
+  attributes: null,
+  uri : null
+}
+
 
 const NFTForm = () => {
 
   const dispatch = useDispatch();
 
+  const [data, setData] = useState<Data>(initialData);
+
+
+  const [loading, setLoading] = useState(false);
+  const [needURI, setneedURI] = useState(false);
+  const [needRange, setNeedrange] = useState(false);
+
+  const [url, setUri] = useState("");
+
+
+  const fetchData = async  (contractAdrs : string) => {
+
+    setData(initialData) 
+    setNeedrange(false)
+    setLoading(true)
+      
+      var web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/92a3eada72834b629e28ff80ba4af4d0'))  
+
+      const uri = `https://api.etherscan.io/api?module=contract&action=getabi&address=${contractAdrs}&apikey=WKEB4C6A8MPPIYF5699I3A1ZEII57MXG2A`
+      const abii = await fetch(uri)
+      const abi = await abii.json()   
+      
+      var MyContract = new web3.eth.Contract(JSON.parse(abi.result), contractAdrs)
+      console.log("asset ", MyContract)
+
+      const name = await MyContract.methods.name().call();
+      const totalSupply = await MyContract.methods.totalSupply().call();
+      console.log("asset ", name)
+      console.log("asset ", totalSupply)
+
+      setData(pre => {return {...pre, totalSupply, name }}) 
+
+      try{
+        console.log("asset ", "trying 1")
+        const tokenURI1 = await MyContract.methods.tokenURI(1).call();
+        let baseTokenURI1 = tokenURI1.substring(0, tokenURI1.lastIndexOf('/'))
+        baseTokenURI1 = baseTokenURI1?.replace("https", "http")
+        console.log("asset ", baseTokenURI1)
+        setData(pre => {return {...pre, baseTokenURI: baseTokenURI1}}) 
+        setneedURI(false)
+        setNeedrange(true)
+        setLoading(false)
+
+        
+      }  catch(error){    
+            console.log("asset ", "Error fetching URI from useNFT hook too")
+            alert("Please provide NFT URI")
+            setData(pre => {return {...pre,  baseTokenURI: null}}) 
+            setneedURI(true)
+            setLoading(false)
+      }
+  }
+
+
+  const fetchURI = (uri: string) => {
+    setData(pre => {return {...pre, baseTokenURI:  uri}}) 
+  }
+
+
   return (
-    <div> 
-    <Formik 
-   initialValues={{
-     address: '',
-       }}  
-  onSubmit={(values, { setSubmitting }) => {3
-    
-    dispatch(setUploadedContractAddress(values.address))
+   <div className="parent-container"> 
 
-   setTimeout(() => {
-     alert(JSON.stringify(values, null, 2));
-     setSubmitting(false);
-   }, 400);
- }}
+    <div className="form-container" >
 
->
+    <Formik initialValues={{ address: '', uri: '' }}  
+            onSubmit={async (values, { setSubmitting, resetForm, setFieldValue,  }) => {
+            
+              if(!values.uri){
+                dispatch(setUploadedContractAddress(values.address))
+                fetchData(values.address)    
+                // resetForm()
+                setSubmitting(false)
+              }
+              if(values.uri){
+                // setNeedrange(false)
+                setData(pre => {return {...pre, baseTokenURI:  values.uri}}) 
+                setFieldValue(values.uri,"")
+                // resetForm()
+                setneedURI(false)
+                setSubmitting(false)
+                setNeedrange(true)
+              }
+            }}>
 
- {(handleChange) => (
-   <Form>
-     <br />
+    {() => (
+      <Form>
+        <Grid container spacing={2}>
 
-     <Grid container spacing={1} className="form">
-       <Grid item xs={12} className="form-component">
-         <Field
-           component={TextField}
-           type="text"
-           name="address"
-           label="Conrtact Address"
-           fullWidth
-         />
-       </Grid>
+          <Grid item xs={12} >
+            <Field
+              component={TextField}
+              type="text"
+              name="address"
+              label="Conrtact Address"
+              fullWidth
+            />
+          </Grid>
 
-       {/* <Grid item xs={6}>
-         <Field
-           component={TextField}
-           type="number"
-           name="to"
-           label="To"
-           fullWidth
-         />
-       </Grid>
-
-       <Grid item xs={6}>
-         <Field
-           component={TextField}
-           type="number"
-           name="from"
-           label="From"
-           fullWidth
-         />
-       </Grid> */}
+       {
+         needURI ? 
+        //  <div className="form-component">
+          <Grid item xs={12}>
+              <Field
+                component={TextField}
+                type="text"
+                name="uri"
+                label={`NFT URI ${(needURI ? "(Needed)" : "(Optional)")}`}
+                fullWidth
+              />
+          </Grid> 
+          // </div>
+          : 
+          null
+       } 
 
         <div className="form-button-container">
-         <Button
-           variant="contained"
-           color="primary"
-           type="submit"
-           className="form-button"
-         >
-           <div >Load</div>
-         </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            type="submit"
+            className="form-button"
+          >
+            {
+              needURI ?
+                  <div >Enter URI </div> :    
+                  <div >Load Contract</div>
+            }
+          </Button>
+         </div>
 
-       </div>
+
+       {
+         loading ? 
+           <div> Loading . . . </div> : 
+           <div className="project-detail-container">
+
+              {
+                data?.name || data?.totalSupply || data?.baseTokenURI ?
+                <div className="project-detail-header"> Project Details </div> :
+                null
+              }
+              {
+                data?.name ? 
+                <div> Name : {data?.name} </div> :
+                null
+              }
+              {
+                data?.totalSupply ? 
+                <div> TotalSupply : {data?.totalSupply} </div>  :
+                null
+              }
+              {
+                data?.baseTokenURI ? 
+                <div> BaseTokenURI : {data?.baseTokenURI} </div> : 
+                null
+              }
+
+          </div>
+        }
+
+
      </Grid>
    </Form>
  )}
  
 </Formik> 
+
+
+</div>
+
+
+    <div className="form-container" >
+
+{
+      needRange ?
+        <Formik initialValues={{ from: 0, to: 20 }}  
+            onSubmit={async (values, { setSubmitting, resetForm, setFieldValue,  }) => {}}
+            >
+            {() => (
+              <Form>    
+                {/* <div className="sub-container"> */}
+                  <div className="range-header"> Enter Range you want to snip </div>
+                  <Grid container spacing={4} >
+
+
+                  <Grid item xs={6}>
+                    <Field
+                      component={TextField}
+                      type="number"
+                      name="from"
+                      label="From"
+                      fullWidth
+                      />
+                  </Grid>
+
+                  <Grid item xs={6}>
+                    <Field
+                      component={TextField}
+                      type="number"
+                      name="to"
+                      label="To"
+                      fullWidth
+                      />
+                  </Grid>
+
+                  <div className="form-button-container">
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      type="submit"
+                      className="form-button"
+                      >
+                    <div >Snip </div> 
+
+                    </Button>
+                  </div>
+        
+                </Grid>  
+                
+              {/* </div> */}
+              </Form>
+            )}
+        </Formik> :
+        null
+    }
+
+            
+      </div>
+
 
     </div>
 
@@ -92,37 +270,3 @@ const NFTForm = () => {
 
 export default NFTForm;
 
-{/* <div>
-<div className="form-text"> Snip an NFT </div> */}
-
-
-
-
-
-
-    // const initialValues: MyFormValues = { address: '' };
-    // return (
-    //   <div>
-    //     <h1>My Example</h1>
-    //     <Formik
-    //       initialValues={initialValues}
-    //       onSubmit={(values, actions) => {
-    //         console.log({ values, actions });
-    //         alert(JSON.stringify(values, null, 2));
-    //         actions.setSubmitting(false);
-    //       }}
-    //     >
-    //       <Form>
-    //       <Grid item xs={12} className="form-component">
-    //             <Field
-    //               component={TextField}
-    //               type="text"
-    //               name="address"
-    //               label="Conrtact Address"
-    //               fullWidth
-    //             />
-    //           </Grid>
-
-    //         <button type="submit">Load</button>
-    //       </Form>
-    //     </Formik>
--- src/components/PostSinginScreen/components/nftCards/index.tsx
+++ src/components/PostSinginScreen/components/nftCards/index.tsx
@@ -11,6 +11,7 @@ import { useSelector } from 'react-redux';
 // import Button from "@mui/material/Button";
 // import App from '../nftCardModel'
 const Web3 = require("web3");
+import { useNft } from "use-nft"
 
 
 // import { OpenSeaPort, Network  } from 'opensea-js'
@@ -29,7 +30,12 @@ const NFTCards = () => {
   
   // const seaport = new OpenSeaPort(provider, { networkName: Network.Main });
 
-  useEffect(() => {
+  // const reTrying = () => {
+
+  // }
+
+
+  // useEffect(() => {
 
     const fetchData = async () => {
       
@@ -47,16 +53,68 @@ const NFTCards = () => {
 
       const name = await MyContract.methods.name().call();
       const totalSupply = await MyContract.methods.totalSupply().call();
-      const tokenURI = await MyContract.methods.tokenURI(1).call();
-      const baseTokenURI = tokenURI.substring(0, tokenURI.lastIndexOf('/'))
+      console.log("asset ", name)
+      console.log("asset ", totalSupply)
+
+
+      try{
+        console.log("asset ", "trying 1")
+
+        const tokenURI1 = await MyContract.methods.tokenURI(1).call();
+        let baseTokenURI1 = tokenURI1.substring(0, tokenURI1.lastIndexOf('/'))
+        baseTokenURI1 = baseTokenURI1?.replace("https", "http")
+        console.log("asset ", baseTokenURI1)
+        setData(pre => {return {...pre, totalSupply, name, baseTokenURI: baseTokenURI1}}) 
+        
+      } catch(error) {
+        console.log("asset Error fetching URI from ABI. Now trying from useNFT hook")
+        
+        try{
+          console.log("asset ", "trying 2")
+          
+          const {nft } = useNft(
+            contractAdrs,
+            "2"
+            )
+            console.log("asset ", nft)
+            
+            const tokenURI2 = nft?.metadataUrl
+            console.log("asset ", tokenURI2)
+
+            if(tokenURI2){
+              console.log("asset ", tokenURI2)
+              let baseTokenURI2 = tokenURI2.substring(0, tokenURI2.lastIndexOf('/'))
+              baseTokenURI2 = baseTokenURI2?.replace("https", "http")
+              setData(pre => {return {...pre, totalSupply, name, baseTokenURI: baseTokenURI2}})
+              console.log("asset ", baseTokenURI2)
+              
+            }
+            
+          } catch(error){    
+            console.log("asset ", "Error fetching URI from useNFT hook too")
+            alert("Please provide NFT URI")
+            
+          }
+          
+        }
+
+
+          // console.log("asset ", "trying 2")
+                
+          // const {nft } = await useNft(
+          //   uploadedContractAddress,
+          //   "2"
+          //   )
+          //   console.log("asset ", nft)
+            
+          //   const tokenURI2 = nft?.metadataUrl
+          //   console.log("asset", tokenURI2)
+    
 
       // const tokenuri = tokenURI.replace("https", "http");
       // const asset = await fetch(tokenuri)
       // const data = await asset.json()
 
-      console.log("asset ", tokenURI.replace("https", "http"))
-
-      setData(pre => {return {...pre, totalSupply, name, baseTokenURI}})
 
       // attributes: data.attributes
       // const asset = await seaport.api.getAsset({
@@ -65,8 +123,8 @@ const NFTCards = () => {
       // })
 
     }
-    fetchData()
-  }, [uploadedContractAddress])
+    // fetchData()
+  // }, [uploadedContractAddress])
 
   return (
     <div className="cards-container">
@@ -75,17 +133,21 @@ const NFTCards = () => {
        
       
 
-        <div> Name : {data?.name} </div>
+        {/* <div> Name : {data?.name} </div>
         <div> TotalSupply : {data?.totalSupply} </div>
-        <div> BaseTokenURI : {data?.baseTokenURI} </div>
+        <div> BaseTokenURI : {data?.baseTokenURI} </div> */}
         {/* <div> Attributes : {JSON.stringify(data?.attributes.trait_type)} </div> */}
 
-        
-
-        
+        <br />
+        <div> Crypto Kitties: 0x06012c8cf97bead5deae237070f9587f8e7a266d </div>
+        <div> Crypto Punks: 0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb </div>
+        <div> BearsOnTheBlock: 0x02aa731631c6d7f8241d74f906f5b51724ab98f8 </div>
+        <div> Testing: 0x2cfcbf304415d87611E89fd284Ed362Cf6bA5141 </div>
+        <div> Testing: 0xA66CC78067fd1E6Aa3eEC4CcdFF88D81527F92c1 </div>
 
 
     </div>
+
 
   );
 };
