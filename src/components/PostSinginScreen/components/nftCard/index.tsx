17a5b59a198beef38e94fbd2cd1e16d29791e8f5
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2021-10-03 04:14:21 +0500

    changes

---
+++ src/components/PostSinginScreen/components/nftCard/index.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+interface Props{
+    image:string,
+    name?: string,
+    tokenID: string,
+    rarity_score: number
+}
+
+const NFTCard: React.FC<Props> = ({image, name, tokenID, rarity_score }) => {
+  return (
+    <Card sx={{ height: 400, width: 300 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="300"
+          image={image}
+          alt={name? name: "nothing"}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {name? name: "No name avaiable"}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Token ID: {tokenID}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Rarity Score: {rarity_score}
+          </Typography>
+
+          
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
+
+export default NFTCard;
--- src/components/PostSinginScreen/components/nftCards/index.tsx
+++ src/components/PostSinginScreen/components/nftCards/index.tsx
@@ -14,23 +14,20 @@ const Web3 = require("web3");
 // import { useNft } from "use-nft"
 import Switch from '@mui/material/Switch';
 import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
+import NFTCard from "../nftCard";
 
 
 // import { OpenSeaPort, Network  } from 'opensea-js'
 
-// interface Data {
-//   totalSupply: string, 
-//   name: string,
-//   baseTokenURI: string,
-//   // attributes: any
-// }
 
 const NFTCards = () => {
   
   const dispatch = useDispatch();
   const [normalization, setNormalization] = useState(true)
+  const [showNFTs, setShowNFTs] = useState(false)
   const { countOfAllAttribute, projectInfo, rarityScoreOfAllValues, rarityScoreOfAllAttributes, allAvailableAttributes, list_of_all_tokens } = useSelector((state: any) => state);
 
 
@@ -38,6 +35,7 @@ const NFTCards = () => {
 
   const handleChange = () => {
     setNormalization(!normalization)
+    findRarityScore()
   }
 
   const findRarityScore = () => {
@@ -104,122 +102,138 @@ const NFTCards = () => {
         })
       })
     }
+    setShowNFTs(true)
 
   }
-
-
-    
+ 
 
   return (
     <div className="cards-container">
       <div className="cards-header"> NFTs </div>
   
-      {/* <Switch onChange={handleChange} /> <br /> */}
+       <Grid container >
 
-      <FormGroup>
-          <FormControlLabel onChange={handleChange} control={<Switch defaultChecked />} label="Normalization" />
-      </FormGroup> <br />
-
-
-      <Button onClick={findRarityScore} variant="contained"> Check rarity </Button>
 
       {
-          countOfAllAttribute ?
-          <div>
-                  <div> Total Variations in Attributes </div> 
-          {
-              countOfAllAttribute.map((attribute: CountOfEachAttribute, key: number) => {
-                return (
-                  <div key= {key}><b> {key+1}:  {attribute.trait_type}  <span> {attribute.total_variations} </span> </b></div>
-              )
-          })}
-
-          <br />
-          <br />
-          
-          </div> : null
-        }
-
-
-        {
-          countOfAllAttribute ? 
-          <div>
-                             <div> Count of each trait </div> 
-                             <br />
-                             <br />
-
-              {
-                countOfAllAttribute.map((attribute: CountOfEachAttribute) => {
-
-                  return (
-                    <div>   
-                          <span><b> {attribute.trait_type} {":"}</b></span>
-                            {
-                              attribute.trait_count && attribute.trait_count.map((attribute_count: TraitCount, key: number) => {
-                                return (
-                                <span key= {key}> {attribute_count.value}:  {attribute_count.count}  </span>
-                                )
-                              })
-                            }  
-                      <br />
-                      <br />
-                    </div>
-                  )
-                })
-              }
-          </div> : 
-          null
-        }
-
-        {/* {
-          list_of_all_tokens !== null  ?
+        showNFTs ? 
+        <Grid item xs={9} style={{display: "flex"}}>
           <>
           {
-            list_of_all_tokens.map((attributesOfEachToekn: AttributesOfEachToekn, key: number) => {
-              if(attributesOfEachToekn)
-              return (
-              <div key={key}>
-                  <div> Attributes of Token : {attributesOfEachToekn.tokenID}</div> 
-                  {
-                    attributesOfEachToekn.attributes.map((attribute: Attribute, key: number) => {
-                          return (
-                            <span key= {key}> {`${attribute.trait_type}: ${attribute.value}    `} </span>
-                        )
-                    })
-                  }
-                  <br />
-                  <br />
-             </div>
-              )
-          })} 
-          
-          </> : null
-        }*/}
-
-
-
-
-        <br />
-        <br />
-        <br />
-        <br />
-        <div> Crypto Kitties: 0x06012c8cf97bead5deae237070f9587f8e7a266d </div>
-        <div> Crypto Punks: 0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb </div>
-        <div> BearsOnTheBlock: 0x02aa731631c6d7f8241d74f906f5b51724ab98f8 </div>
-        <div> Testing: 0x2cfcbf304415d87611E89fd284Ed362Cf6bA5141 </div>
-        <div> Testing: 0xA66CC78067fd1E6Aa3eEC4CcdFF88D81527F92c1 </div>
-        <div> WannabeMusicClub: 0x402491a577373995fF3382e6da3c282cb0564902 </div>
-        <div> lostboy: 0xc1a1e381389cb927f1d57511e144b644ef0c6385 </div>
-
+             list_of_all_tokens && list_of_all_tokens.map((token: AttributesOfEachToekn) => {
+                return (
+                  <NFTCard image={token.image} name={token.name} tokenID={token.tokenID} rarity_score={token.rarity_score} />
+                )            
+            })
+          }
+          </>
         
+        </Grid> 
+        : <Grid item xs={9}>  Welcome </Grid>
+        }
 
-        
+        <Grid item xs={2}> 
 
+      
+        <FormGroup>
+            <FormControlLabel onChange={handleChange} control={<Switch defaultChecked />} label="Normalization" />
+            <Button onClick={findRarityScore} variant="contained"> Check rarity </Button>
+        </FormGroup>
+        </Grid>
+
+        </Grid>
 
     </div>
-
 
   );
 };
 
 export default NFTCards;
+
+{/* {
+    countOfAllAttribute ?
+    <div>
+            <div> Total Variations in Attributes </div> 
+    {
+        countOfAllAttribute.map((attribute: CountOfEachAttribute, key: number) => {
+          return (
+            <div key= {key}><b> {key+1}:  {attribute.trait_type}  <span> {attribute.total_variations} </span> </b></div>
+        )
+    })}
+
+    <br />
+    <br />
+    
+    </div> : null
+  } */}
+
+
+
+  {/* {
+    countOfAllAttribute ? 
+    <div>
+                       <div> Count of each trait </div> 
+                       <br />
+                       <br />
+
+        {
+          countOfAllAttribute.map((attribute: CountOfEachAttribute) => {
+
+            return (
+              <div>   
+                    <span><b> {attribute.trait_type} {":"}</b></span>
+                      {
+                        attribute.trait_count && attribute.trait_count.map((attribute_count: TraitCount, key: number) => {
+                          return (
+                          <span key= {key}> {attribute_count.value}:  {attribute_count.count}  </span>
+                          )
+                        })
+                      }  
+                <br />
+                <br />
+              </div>
+            )
+          })
+        }
+    </div> : 
+    null
+  } */}
+
+  {/* {
+    list_of_all_tokens !== null  ?
+    <>
+    {
+      list_of_all_tokens.map((attributesOfEachToekn: AttributesOfEachToekn, key: number) => {
+        if(attributesOfEachToekn)
+        return (
+        <div key={key}>
+            <div> Attributes of Token : {attributesOfEachToekn.tokenID}</div> 
+            {
+              attributesOfEachToekn.attributes.map((attribute: Attribute, key: number) => {
+                    return (
+                      <span key= {key}> {`${attribute.trait_type}: ${attribute.value}    `} </span>
+                  )
+              })
+            }
+            <br />
+            <br />
+       </div>
+        )
+    })} 
+    
+    </> : null
+  }*/}
+
+
+
+{/* 
+  <br />
+  <br />
+  <br />
+  <br />
+  <div> Crypto Kitties: 0x06012c8cf97bead5deae237070f9587f8e7a266d </div>
+  <div> Crypto Punks: 0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb </div>
+  <div> BearsOnTheBlock: 0x02aa731631c6d7f8241d74f906f5b51724ab98f8 </div>
+  <div> Testing: 0x2cfcbf304415d87611E89fd284Ed362Cf6bA5141 </div>
+  <div> Testing: 0xA66CC78067fd1E6Aa3eEC4CcdFF88D81527F92c1 </div>
+  <div> WannabeMusicClub: 0x402491a577373995fF3382e6da3c282cb0564902 </div>
+  <div> lostboy: 0xc1a1e381389cb927f1d57511e144b644ef0c6385 </div> */}
